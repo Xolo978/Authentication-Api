bb765582d5019055ba49708fbe4a213ba7155cc3
Xolo978 <prithviraj.banik2024@iem.edu.in>
2024-08-12 22:59:46 +0530

    Made the User schema

---
+++ server/models/User.ts
@@ -0,0 +1,21 @@
+import mongoose, {Document,Schema} from "mongoose";
+export interface User extends Document {
+    username:string;
+    password:string;
+}
+const UserSchema : Schema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique:true
+    },
+    password:{
+        type: String,
+        required: true,
+    }
+
+})
+
+export const User = mongoose.model<User>('User', UserSchema);
+
+
